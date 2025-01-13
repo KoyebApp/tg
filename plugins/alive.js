1a7318a09500aa7985472341769be0acd55f3332
Koyeb App <koyebapp@gmail.com>
2025-01-13 16:33:11 +0500

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -3,6 +3,6 @@ const handler = async ({ bot, m, db }) => {
   await bot.sendMessage(m.chat.id, "Bot is alive and running!");
 };
 
-handler.command = ['alive'];
+handler.command = ['alive', 'help'];
 
 module.exports = handler;
