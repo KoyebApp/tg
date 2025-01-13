c19f42e6db0bba8bab1f7dc6ec25f93c2cf06c1f
Koyeb App <koyebapp@gmail.com>
2025-01-14 02:35:14 +0500

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,15 +1,22 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
 
-// Get the owner's chat ID from the environment variables
 const OWNER_ID = process.env.OWNER_ID;
 
 let handler = async ({ msg, bot, chatId, text }) => {
   try {
+    // Ensure chatId is defined
+    if (!chatId) {
+      throw new Error('chatId is undefined');
+    }
+
+    console.log('Received msg:', msg);  // Debug log to inspect msg object
+    console.log('Received chatId:', chatId);  // Debug log for chatId
+
     // Ensure the command is executed by the owner
     if (chatId.toString() === OWNER_ID) {
       // Notify the user that the bot is updating
-      bot.sendMessage(chatId, "Updating the bot... Please wait...");
+      await bot.sendMessage(chatId, "Updating the bot... Please wait...");
 
       // Execute the git pull command
       let stdout = execSync('git pull' + (text ? ' ' + text : ''));
@@ -18,14 +25,14 @@ let handler = async ({ msg, bot, chatId, text }) => {
       fs.readdirSync('plugins').forEach(v => global.reload('', v));
 
       // Send the output of the git pull command
-      bot.sendMessage(chatId, stdout.toString());
+      await bot.sendMessage(chatId, stdout.toString());
     } else {
       // Send a message if the user is not authorized
-      bot.sendMessage(chatId, "You are not authorized to use this command.");
+      await bot.sendMessage(chatId, "You are not authorized to use this command.");
     }
   } catch (error) {
     console.error("Error during update:", error);
-    bot.sendMessage(chatId, "An error occurred while updating the bot. Please try again later.");
+    await bot.sendMessage(chatId, "An error occurred while updating the bot. Please try again later.");
   }
 };
 
