10fabcb368b5ee4d7a1ccf1b8505e502480d1e40
Koyeb App <koyebapp@gmail.com>
2025-01-13 17:39:57 +0500

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,7 +1,7 @@
 const path = require('path');  // Import the 'path' module to handle file paths
 
 const handler = async ({ bot, m, db }) => {
-  const photoPath = path.join(__dirname, '../assets/A.jpg');  // Ensure the path is correct
+  const photoPath = path.join(__dirname, './assets/A.jpg');  // Ensure the path is correct
 
   // Send a photo with a caption
   try {
