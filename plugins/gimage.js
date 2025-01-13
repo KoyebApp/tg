4bf6b339f520880d674081cf511fe7cb170bd7ca
Koyeb App <koyebapp@gmail.com>
2025-01-13 20:18:12 +0500

    Update gimage.js

--- plugins/gimage.js
+++ plugins/gimage.js
@@ -9,6 +9,8 @@ const handler = async ({ bot, m, text, db, usedPrefix, command, query }) => {
     return bot.sendMessage(chatId, "Please provide a search query.");
   }
 
+  // Send "waiting" message to indicate the bot is processing
+    await bot.sendMessage(chatId, "⏳ Please wait, fetching the images...");
   try {
     // Call your image search API or logic here using the query
     const response = await Qasim.googleImage(query);
