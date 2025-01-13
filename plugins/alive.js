302f6dca9fdb83481f28a6ae0cc635ca5484858f
Koyeb App <koyebapp@gmail.com>
2025-01-13 17:36:33 +0500

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,6 +1,6 @@
 // alive.js plugin example
 const handler = async ({ bot, m, db }) => {
-  const photoPath = './assets/A.jpg';  // Path to your photo
+  const photoPath = '../assets/A.jpg';  // Path to your photo
 
   // Send a photo with a caption
   await bot.sendPhoto(m.chat.id, { source: photoPath }, { caption: 'Bot Is Alive And Running!' });
