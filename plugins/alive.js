857d935fabe27c8e2191f05e2e54011323a2f6b2
Koyeb App <koyebapp@gmail.com>
2025-01-15 17:08:55 +0500

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,12 +1,10 @@
 const path = require('path');  // Import 'path' module to handle file paths
 const fs = require('fs');  // Import 'fs' module to check file existence
 
-const handler = async ({ bot, m, db, text }) => {
-  console.log('Alive command triggered');  // Debugging log to check if it's called
+const handler = async ({ bot, m }) => {
   const photoPath = path.join(__dirname, '../assets/A.jpg');
 
   if (!fs.existsSync(photoPath)) {
-    console.log('Photo not found');  // Debugging log for photo path
     return;
   }
 
@@ -17,8 +15,8 @@ const handler = async ({ bot, m, db, text }) => {
   }
 };
 
+handler.command = ['alive', 'awake'];  // Command list
 handler.help = ['alive', 'awake'];
 handler.tags = ['main'];
-handler.command = ['alive', 'awake'];
 
 module.exports = handler;
