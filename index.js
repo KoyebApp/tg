fa285a20cd05a14b90b2c7efb0b0aa18972b221d
Koyeb App <koyebapp@gmail.com>
2025-01-19 17:11:20 +0500

    Update index.js

--- index.js
+++ index.js
@@ -162,8 +162,8 @@ bot.on('message', (msg) => {
     const handler = plugins[normalizedCommand];
 
     if (handler) {
-      let context = {
-        bot,
+      let context = { 
+        bot, 
         m: msg, 
         text, 
         query, 
