bcde882c36105fb45fc71efcccbb4c3abe5a9d50
Koyeb App <koyebapp@gmail.com>
2025-01-19 16:54:54 +0500

    Update maker-qr.js

--- plugins/maker-qr.js
+++ plugins/maker-qr.js
@@ -1,17 +1,26 @@
 const { toDataURL } = require('qrcode');
-const chatId = m.chat.id
 
 let handler = async ({ m, text, bot }) => {
-  if (!text) throw `*Give a text to convert*`
-  bot.sendPhoto(
-    chatId,
-    await toDataURL(text.slice(0, 2048), { scale: 8 }),
-    'qrcode.png',
-    'Here u go',
-    m
-  )
-}
-handler.help = ['', 'code'].map(v => 'qr' + v + ' <text>')
-handler.tags = ['tools']
-handler.command = /^qr(code)?$/i
-module.exports = handler
+  if (!text) throw `*Give a text to convert*`;
+
+  try {
+    const qrCodeDataUrl = await toDataURL(text.slice(0, 2048), { scale: 8 });
+    
+    bot.sendPhoto(
+      m.chat.id,
+      qrCodeDataUrl,
+      'qrcode.png',
+      'Here you go',
+      m
+    );
+  } catch (err) {
+    throw `Error generating QR code: ${err.message}`;
+  }
+};
+
+// Define help command and command tags
+handler.help = ['', 'code'].map(v => 'qr' + v + ' <text>');
+handler.tags = ['tools'];
+handler.command = /^qr(code)?$/i;
+
+module.exports = handler;
