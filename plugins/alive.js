189568158e231fd9afda9de0f2cff16101d0a287
Koyeb App <koyebapp@gmail.com>
2025-01-21 01:48:36 +0500

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -2,14 +2,14 @@ const path = require('path');  // Import 'path' module to handle file paths
 const fs = require('fs');  // Import 'fs' module to check file existence
 
 const handler = async ({ bot, m }) => {
-  const photoPath = path.join(__dirname, '../assets/A.jpg');
+  const photoPath = path.join(__dirname, '../assets/C.jpg');
 
   if (!fs.existsSync(photoPath)) {
     return;
   }
 
   try {
-    await bot.sendPhoto(m.chat.id, photoPath, { caption: 'Bot Is Alive And Running!' });
+    await bot.sendPhoto(m.chat.id, photoPath, { caption: 'Bot Is Alive And Running!\n\nSupport By Subscribe youtube.com/@GlobalTechInfo\n' });
   } catch (error) {
     console.error('Error sending photo:', error);
   }
